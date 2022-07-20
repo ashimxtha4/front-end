6539e043876d8b3466718040a2670bb8ae46d3a1
ashimxtha4 <ashim.xtha2000@gmail.com>
2022-07-20 23:03:19 +0545

    sidebar change

--- src/Components/MyRoutes.js
+++ src/Components/MyRoutes.js
@@ -5,11 +5,13 @@ import Task from '../Pages/Task'
 import Notice from '../Pages/Notice'
 import Events from '../Pages/Events'
 import Profile from '../Pages/Profile'
+import DashBoard from '../Pages/DashBoard'
 
 const MyRoutes = () => {
   return (
     <Routes>
-    <Route path='/' element={<Projects />} />
+    <Route path='/' element={<DashBoard />} />
+    <Route path='/projects' element={<Projects />} />
     <Route path='/task' element={<Task />} />
     <Route path='/notiec' element={<Notice />} />
     <Route path='/events' element={<Events />} />
