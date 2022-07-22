8e8cf1d2a575d0fef8fbf3e5d9f113f6f89168e3
aster-yk <yubin.karki@asterdio.com>
2022-07-22 12:53:24 +0545

    Edited project component files.

--- src/Components/ProjectComponent.jsx
+++ src/Components/ProjectComponent.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ProjectTaskDetails from "./ProjectTaskDetails";
 import "../Styles/ProjectComponent.css";
 
-const ProjectComponent = () => {
+const ProjectComponent = (props) => {
   return (
     <div className="project-main-div">
       <div className="project-title-div">
-        <h2>Title Prop</h2>
+        <h2>{props.cardtitle}</h2>
       </div>
 
       <div className="project-body-div">
         <div className="project-body-top">
-          <span>Overall</span>
-          <span>Currently</span>
+          <span>{props.cardtabtitle1}</span>
+          <span>{props.cardtabtitle2}</span>
         </div>
         <div className="project-body-bot">
           <ProjectTaskDetails />
--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ProjectComponent from "../Components/ProjectComponent";
+import DashboardHome from "./DashboardHome";
 
 const DashBoard = () => {
   return (
-    <>
-      <ProjectComponent />
+    <>    
+      <DashboardHome />
     </>
   );
 };
