0765bfd2770293869f52c39226fe8bac557509ae
aster-yk <yubin.karki@asterdio.com>
2022-07-24 08:44:21 +0545

    Added MyTaskComponent in the first body div.

--- src/Pages/DashboardHome.js
+++ src/Pages/DashboardHome.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ProjectComponent from "../Components/ProjectComponent";
 import EventComponent from "../Components/EventComponent";
+import NoticeComponent from "../Components/NoticeComponent";
 import "../Styles/DashboardHome.css"
 import TeamMemberComponent from "../Components/TeamMemberComponent";
+import MyTaskComponent from "../Components/MyTaskComponent";
 
 const DashboardHome = () => {
   return (
@@ -11,12 +13,13 @@ const DashboardHome = () => {
         <h2>Welcome, User</h2>
       </div>
       <div class="body-card-div-1">
-        <ProjectComponent cardtitle="My Tasks" cardtabtitle1="Overall" cardtabtitle2="To do"/>
+        <MyTaskComponent cardtitle="My Tasks" cardtabtitle1="Ok" cardtabtitle2="Nice"/>
         <ProjectComponent cardtitle="My Projects" cardtabtitle1="Overall" cardtabtitle2="Currently"/>
         <TeamMemberComponent cardtitle="Team Members" membername="Nice Name" memberposition="UI/UX Designer"/>
       </div>
       <div class="body-card-div-2">
         <EventComponent />
+        <NoticeComponent />
       </div>
     </div>
   );
