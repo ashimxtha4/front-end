6708a35bc4aaa61bd5f2c4d57e87ad91ce85eff0
ashimxtha4 <ashim.xtha2000@gmail.com>
2022-07-25 12:13:23 +0545

    done all changes 25/7

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -9,54 +9,54 @@ const Profile = () => {
   return (
     <>
     <div className='tempelate-div'>
-    <div className='side-div'>
-      <SideBar />
-    </div>
-    <div className='content-div'>
-    <div className="profile-main-div">
-      <div className="profile-card-div">
-        <div className="profile-card-top-div">
-          <div className="profile-top-img">
-            <img src={profilepicmd} alt="" className="pofile-img"></img>
-          </div>
-        </div>
-        <form className="profile-card-mid-form" action="" method="">
-          <div className="profile-mid-left-div">
-            <div className="profile-mid-input-div">
-              <label for="firstName">First Name</label><br></br>
-              <input type="text" name="firstName"></input>
-            </div>
-            <div className="profile-mid-input-div">
-              <label for="emailAddress">Email Address</label><br></br>
-              <input type="email" name="emailAddress" placeholder="previous@mail.com" disabled></input>
-            </div>
-            <div className="profile-mid-input-div">
-              <label for="phoneNumber">Phone Number</label><br></br>
-              <input type="text" name="phoneNumber"></input>
+      <div className='side-div'>
+        <SideBar />
+      </div>
+      <div className='content-div'>
+        <div className="profile-main-div">
+        <div className="profile-card-div">
+          <div className="profile-card-top-div">
+            <div className="profile-top-img">
+              <img src={profilepicmd} alt="" className="pofile-img"></img>
             </div>
           </div>
-          <div className="profile-mid-right-div">
-            <div className="profile-mid-input-div">
-              <label for="lastName">Last Name</label><br></br>
-              <input type="text" name="firstName"></input>
+          <form className="profile-card-mid-form" action="" method="">
+            <div className="profile-mid-left-div">
+              <div className="profile-mid-input-div">
+                <label for="firstName">First Name</label><br></br>
+                <input type="text" name="firstName"></input>
+              </div>
+              <div className="profile-mid-input-div">
+                <label for="emailAddress">Email Address</label><br></br>
+                <input type="email" name="emailAddress" placeholder="previous@mail.com" disabled></input>
+              </div>
+              <div className="profile-mid-input-div">
+                <label for="phoneNumber">Phone Number</label><br></br>
+                <input type="text" name="phoneNumber"></input>
+              </div>
             </div>
-            <div className="profile-mid-input-div">
-              <label for="password">Password</label><br></br>
-              <input type="password" name="password"></input>
+            <div className="profile-mid-right-div">
+              <div className="profile-mid-input-div">
+                <label for="lastName">Last Name</label><br></br>
+                <input type="text" name="firstName"></input>
+              </div>
+              <div className="profile-mid-input-div">
+                <label for="password">Password</label><br></br>
+                <input type="password" name="password"></input>
+              </div>
+              <div className="profile-mid-input-div">
+                <label for="role">Role</label><br></br>
+                <input type="text" name="role"></input>
+              </div>
             </div>
-            <div className="profile-mid-input-div">
-              <label for="role">Role</label><br></br>
-              <input type="text" name="role"></input>
-            </div>
+          </form>
+          <div className="profile-card-bot-div">
+            <button className="bot-save-btn">Save</button>
+            <button className="bot-cancel-btn">Cancel</button>
           </div>
-        </form>
-        <div className="profile-card-bot-div">
-          <button className="bot-save-btn">Save</button>
-          <button className="bot-cancel-btn">Cancel</button>
         </div>
       </div>
-    </div>
-    </div>
+      </div>
     </div>
     </>
     
