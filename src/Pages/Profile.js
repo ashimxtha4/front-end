0c0e186aaa1b4153ba0d8fda530a14cb29323209
aster-yk <yubin.karki@asterdio.com>
2022-07-26 10:16:13 +0545

    Added number validation in profile phone field.

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Styles/Profile.css";
-import profilepicmd from "../Images/profilepicmedium.png"
+import profilepicmd from "../Images/profilepicmedium.png";
 
 const Profile = () => {
   return (
@@ -14,29 +14,47 @@ const Profile = () => {
         <form className="profile-card-mid-form" action="" method="">
           <div className="profile-mid-left-div">
             <div className="profile-mid-input-div">
-              <label for="firstName">First Name</label><br></br>
+              <label for="firstName">First Name</label>
+              <br></br>
               <input type="text" name="firstName"></input>
             </div>
             <div className="profile-mid-input-div">
-              <label for="emailAddress">Email Address</label><br></br>
-              <input type="email" name="emailAddress" placeholder="previous@mail.com" disabled></input>
+              <label for="emailAddress">Email Address</label>
+              <br></br>
+              <input
+                type="email"
+                name="emailAddress"
+                placeholder="previous@mail.com"
+                disabled
+              ></input>
             </div>
             <div className="profile-mid-input-div">
-              <label for="phoneNumber">Phone Number</label><br></br>
-              <input type="text" name="phoneNumber"></input>
+              <label for="phoneNumber">Phone Number</label>
+              <br></br>
+              <input
+                type="number"
+                name="phoneNumber"
+                autoComplete="off"
+                pattern="[1-9]{1}[0-9]{9}"
+                onkeypress="return /[0-9]/i.test(event.key)"
+                maxLength={10}
+              ></input>
             </div>
           </div>
           <div className="profile-mid-right-div">
             <div className="profile-mid-input-div">
-              <label for="lastName">Last Name</label><br></br>
+              <label for="lastName">Last Name</label>
+              <br></br>
               <input type="text" name="firstName"></input>
             </div>
             <div className="profile-mid-input-div">
-              <label for="password">Password</label><br></br>
+              <label for="password">Password</label>
+              <br></br>
               <input type="password" name="password"></input>
             </div>
             <div className="profile-mid-input-div">
-              <label for="role">Role</label><br></br>
+              <label for="role">Role</label>
+              <br></br>
               <input type="text" name="role"></input>
             </div>
           </div>
