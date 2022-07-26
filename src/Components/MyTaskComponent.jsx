0d36e3784efc5fcffee6d72fcad79f0e26c18e7f
aster-yk <yubin.karki@asterdio.com>
2022-07-26 13:15:22 +0545

    Added state change for My Tasks component buttons.

--- src/Components/MyTaskComponent.jsx
+++ src/Components/MyTaskComponent.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import MyTaskDetails from "./MyTaskDetails";
 import "../Styles/MyTaskComponent.css";
 
+const taskArrayOverall = [
+  {
+    title: "Ov Title 1",
+    date: "2022-01-01",
+  },
+  {
+    title: "Ov Title 2",
+    date: "2022-01-02",
+  },
+  {
+    title: "Ov Title 3",
+    date: "2022-01-03",
+  },
+];
+
+const taskArrayOwn = [
+  {
+    title: "Own Title 1",
+    date: "2022-01-01",
+  },
+  {
+    title: "Own Title 2",
+    date: "2022-01-02",
+  },
+  {
+    title: "Own Title 3",
+    date: "2022-01-03",
+  },
+];
+
 const MyTaskComponent = (props) => {
+  const [cardStatus, setState] = useState("overall");
+
+  const changeOverAll = () => {
+    setState("overall");
+  };
+
+  const changeToDo = () => {
+    setState("todo");
+  };
+
   return (
     <div className="mytask-main-div">
       <div className="mytask-title-div">
@@ -11,11 +51,31 @@ const MyTaskComponent = (props) => {
 
       <div className="mytask-body-div">
         <div className="mytask-body-top">
-          <button>{props.cardtabtitle1}</button>
-          <button>{props.cardtabtitle2}</button>
+          <button onClick={changeOverAll}>{props.cardtabtitle1}</button>
+          <button onClick={changeToDo}>{props.cardtabtitle2}</button>
         </div>
         <div className="mytask-body-bot">
-          <MyTaskDetails  tasktitle="Make design for login page" taskdate="12th July"/>
+          {cardStatus === "overall"
+            ? // eslint-disable-next-line array-callback-return
+              taskArrayOverall.map((item, index) => {
+                if (index < 2)
+                  return (
+                    <MyTaskDetails
+                      tasktitle={item.title}
+                      taskdate={item.date}
+                    />
+                  );
+              })
+            // eslint-disable-next-line array-callback-return
+            : taskArrayOwn.map((item, index) => {
+                if (index < 2)
+                  return (
+                    <MyTaskDetails
+                      tasktitle={item.title}
+                      taskdate={item.date}
+                    />
+                  );
+              })}
         </div>
 
         <div className="mytask-footer-div">
