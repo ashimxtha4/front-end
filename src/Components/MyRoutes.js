b16196e0deae6e6212210e21a872369655dcd130
aster-yk <yubin.karki@asterdio.com>
2022-08-01 10:00:51 +0545

    Added admin project /admin/project route.

--- src/Components/MyRoutes.js
+++ src/Components/MyRoutes.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Projects from '../Pages/Project'
-import Task from '../Pages/Task'
-import Profile from '../Pages/Profile'
-import DashBoard from '../Pages/DashBoard'
-import SignUp from '../Pages/SignUp';
-import LogIn from '../Pages/LogIn'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Projects from "../Pages/Project";
+import Task from "../Pages/Task";
+import Profile from "../Pages/Profile";
+import DashBoard from "../Pages/DashBoard";
+import SignUp from "../Pages/SignUp";
+import LogIn from "../Pages/LogIn";
+import AdminProject from "../Pages/Admin/AdminProject";
 
 const MyRoutes = () => {
   return (
     <Routes>
-    
-    <Route path='/dashboard' element={<DashBoard />} />
-    <Route path='/projects' element={<Projects />} />
-    <Route path='/task' element={<Task />} />
-    <Route path='/profile' element={<Profile />} />
-    <Route path='/signup' element={<SignUp />}/>
-    <Route path='/' element={<LogIn />}/>
-  </Routes>
-  )
-}
+      <Route path="/dashboard" element={<DashBoard />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/task" element={<Task />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/" element={<LogIn />} />
+      <Route path="/admin/project" element={<AdminProject />} />
+    </Routes>
+  );
+};
 
-export default MyRoutes
+export default MyRoutes;
