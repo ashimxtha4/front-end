05c24348736f35ca94d6efe13bee0f3ea447b820
aster-yk <yubin.karki@asterdio.com>
2022-08-01 10:02:48 +0545

    Added form active and inactive css. Incomplete.

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../Components/SideBar";
 import "./Tempelate.css";
 import "../Styles/Profile.css";
 import profilepicmd from "../Images/profilepicmedium.png";
 
-const Profile = () => {
-  /* const formChange = () => {
-    let btnSaveClass = document.getElementById("bot-save-btn")
-    let btnCancelClass = document.getElementById("bot-cancel-btn")
+const userData = [
+  {
+    firstName: "First",
+    lastName: "Last",
+    email: "email@email.com",
+    password: "password",
+    phone: 8888888,
+    designation: "Front-End Developer",
+  },
+  {
+    firstName: "First",
+    lastName: "Last",
+    email: "email@email.com",
+    password: "password",
+    phone: 8888888,
+    designation: "Front-End Developer",
+  },
+  {
+    firstName: "First",
+    lastName: "Last",
+    email: "email@email.com",
+    password: "password",
+    phone: 8888888,
+    designation: "Front-End Developer",
+  },
+];
 
-    btnSaveClass.classList.replace("bot-save-btn-act")
-    btnCancelClass.classList.replace("bot-cancel-btn-act")
-  } */
+const Profile = () => {
+  // const [formChangeState, setformChange] = useState(""); // Empty string results in false.
+  const [formChangeState, setformChange] = useState(false); // Empty string results in false.
+
+  console.log(formChangeState);
+  const formChange = () => {
+    let btnSaveClass = document.getElementById("bot-save-btn");
+    let btnCancelClass = document.getElementById("bot-cancel-btn");
+
+    // !!formChangeState
+    // formChangeState.length >= 0
+    formChangeState === true
+      ? (btnSaveClass.className = "bot-save-btn-act") &&
+        (btnCancelClass.className = "bot-cancel-btn-act")
+      : (btnSaveClass.className = "bot-save-btn-inact") &&
+        (btnCancelClass.className = "bot-cancel-btn-inact");
+  };
+
+  /* useEffect(() => {
+    effect
+    return () => {
+      cleanup
+    };
+  }, [input]); */
 
   return (
     <>
@@ -43,8 +86,19 @@ const Profile = () => {
                     <input
                       type="text"
                       name="firstName"
-                      placeholder="Yubin"
-                    ></input>
+                      id="firstName"
+                      value={userData[0].firstName}
+                      // onChange={(e) => {
+                      //   setformChange(e.target.value);
+                      //   formChange();
+                      // }}
+                      onKeyDown={() => {
+                        setformChange(true);
+                        formChange();
+                      }}
+                    >
+                      {/* {userData[0].firstName} */}
+                    </input>
                   </div>
                   <div className="profile-mid-input-div">
                     <label for="emailAddress">Email Address</label>
@@ -63,7 +117,6 @@ const Profile = () => {
                       type="number"
                       name="phoneNumber"
                       autoComplete="off"
-                      placeholder="9808584035"
                       pattern="[1-9]{1}[0-9]{9}"
                       onkeypress="return /[0-9]/i.test(event.key)"
                     ></input>
@@ -75,18 +128,17 @@ const Profile = () => {
                     <br></br>
                     <input
                       type="text"
-                      name="firstName"
-                      placeholder="Karki"
+                      name="lastName"
+                      onKeyDown={() => {
+                        setformChange(true);
+                        formChange();
+                      }}
                     ></input>
                   </div>
                   <div className="profile-mid-input-div">
                     <label for="password">Password</label>
                     <br></br>
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="nice"
-                    ></input>
+                    <input type="password" name="password"></input>
                   </div>
                   <div className="profile-mid-input-div">
                     <label for="designation">Designation</label>
@@ -109,8 +161,18 @@ const Profile = () => {
                 </div>
               </form>
               <div className="profile-card-bot-div">
-                <button className="bot-save-btn-act">Edit</button>
-                <button className="bot-cancel-btn-act">Cancel</button>
+                <button className="bot-save-btn-inact" id="bot-save-btn">
+                  Save
+                </button>
+                <button
+                  className="bot-cancel-btn-inact"
+                  id="bot-cancel-btn"
+                  onClick={() => {
+                    setformChange("");
+                  }}
+                >
+                  Cancel
+                </button>
               </div>
             </div>
           </div>
