762792dcd030aaa44afe6ba3c87556642c5485cc
aster-yk <yubin.karki@asterdio.com>
2022-08-04 10:24:45 +0545

    Moved status check from page to action.

--- src/redux/actions/LogInAction.js
+++ src/redux/actions/LogInAction.js
@@ -1,18 +1,27 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import { LOG_IN } from "../container/constant";
 
 const LogInAction = (credentials) => async (dispatch) => {
   try {
     const response = await axios.post(
-      "http://192.168.0.155:3000/user/login",
+      "http://localhost:3000/user/login",
       credentials
     );
     console.log(response.data, "Yo axios post ko response data ho.");
     await dispatch({ type: LOG_IN, payload: response.data });
     localStorage.setItem("response", JSON.stringify(response.data));
+    const user = localStorage.getItem("response");
+    console.log("User from local storage - ", user);
   } catch (error) {
     console.log("LoginAction ko error ->", error.response.data.msg);
-    dispatch({ type: LOG_IN, payload: error.response.data });
+    await dispatch({ type: LOG_IN, payload: error.response.data });
+
+    if (error.response.data.msg === "password is incorrect") {
+      toast.error("Incorrect password");
+    } else if (error.response.data.msg === "email not valid") {
+      toast.error("Not a valid email");
+    }
   }
 };
 
--- src/redux/actions/SignUpAction.js
+++ src/redux/actions/SignUpAction.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 import { SIGN_UP } from "../container/constant";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const SingUpAction = (abc) => async (dispatch) => {
+const SingUpAction = (credentials) => async (dispatch) => {
   try {
-    const response = await axios.post("localhost:3000/user/register", abc);
-    console.log(response.data, "Yo post req to response data ho.");
-    dispatch({ type: SIGN_UP, payload: response.data });
+    const response = await axios.post(
+      "http://localhost:3000/user/register",
+      credentials
+    );
+    console.log(response.data, "Data from axios post request.");
+    await dispatch({ type: SIGN_UP, payload: response.data });
   } catch (err) {
-    console.log(err.response.data.msg);
-    dispatch({ type: SIGN_UP, payload: err.response.data.msg });
+    console.log("Yo signupaction ko catch error ho.", err.response.data.msg);
+    // console.log(err.response.data);
+    await dispatch({ type: SIGN_UP, payload: err.response.data });
+
+    if (err.response.data.status === "false") {
+      toast.error("This email is not available");
+    }
   }
 };
 
